397dfe4c62dee13fbdf528e5430e61c750366d95
Murad Cavadov <13860@holbertonstudents.com>
2026-04-25 17:02:42 +0000

    Fix doctest traceback and pycodestyle

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-Bu modul matrisi bölmək üçün funksiyanı ehtiva edir.
+Bu modul matrisi bolmek ucun funksiyani ehtiva edir.
 """
 
 
 def matrix_divided(matrix, div):
     """
-    Matrisin bütün elementlərini div-ə bölür.
+    Matrisin butun elementlerini div-e bolur.
     """
     msg = "matrix must be a matrix (list of lists) of integers/floats"
     if not isinstance(matrix, list) or not matrix or not matrix[0]:
@@ -18,12 +18,6 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    # NaN və Infinity yoxlaması div üçün
-    if div != div or div == float('inf') or div == -float('inf'):
-        # Əgər div sonsuzluqdursa, bütün nəticələr 0.0 olacaq
-        # Lakin tip yoxlamasını davam etdiririk
-        pass
-
     row_size = len(matrix[0])
     new_matrix = []
 
@@ -32,12 +26,11 @@ def matrix_divided(matrix, div):
             raise TypeError(msg)
         if len(row) != row_size:
             raise TypeError("Each row of the matrix must have the same size")
-        
+
         new_row = []
         for x in row:
             if not isinstance(x, (int, float)):
                 raise TypeError(msg)
-            # Infinity bölməsi Python-da avtomatik 0.0 qaytarır
             new_row.append(round(x / div, 2))
         new_matrix.append(new_row)
 
