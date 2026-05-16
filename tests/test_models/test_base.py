08c67709f0f4a61544a1d19018d5e1cb38ada187
Murad-Cavadov <13860@holbertonstudents.com>
2026-05-16 15:52:34 +0000

    Implement Task 18 create class method for Base class

--- tests/test_models/test_base.py
+++ tests/test_models/test_base.py
@@ -1,36 +1,31 @@
 #!/usr/bin/python3
 """
-Unit tests for the complete Base class including from_json_string method.
+Unit tests for the complete Base class including the create method.
 """
 import unittest
-import os
 from models.base import Base
 from models.rectangle import Rectangle
+from models.square import Square
 
 
-class TestBase(unittest.TestCase):
-    """Test suite for the Base class."""
+class TestBaseCreate(unittest.TestCase):
+    """Test suite for checking the create method of the Base class."""
 
-    def test_auto_id(self):
-        """Test automatic id incrementation."""
-        b1 = Base()
-        b2 = Base()
-        self.assertEqual(b2.id, b1.id + 1)
+    def test_create_rectangle(self):
+        """Test create method for Rectangle instance."""
+        r1 = Rectangle(3, 5, 1, 0, 99)
+        r1_dict = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dict)
+        self.assertEqual(str(r1), str(r2))
+        self.assertIsNot(r1, r2)
 
-    def test_from_json_string_none(self):
-        """Test from_json_string with None argument."""
-        self.assertEqual(Base.from_json_string(None), [])
-
-    def test_from_json_string_empty(self):
-        """Test from_json_string with an empty string."""
-        self.assertEqual(Base.from_json_string(""), [])
-
-    def test_from_json_string_valid(self):
-        """Test from_json_string with a valid JSON string."""
-        json_str = '[{"id": 89, "width": 10, "height": 4}]'
-        output = Base.from_json_string(json_str)
-        self.assertIsInstance(output, list)
-        self.assertEqual(output, [{'id': 89, 'width': 10, 'height': 4}])
+    def test_create_square(self):
+        """Test create method for Square instance."""
+        s1 = Square(5, 2, 2, 88)
+        s1_dict = s1.to_dictionary()
+        s2 = Square.create(**s1_dict)
+        self.assertEqual(str(s1), str(s2))
+        self.assertIsNot(s1, s2)
 
 
 if __name__ == "__main__":
