11af18c3bf65fba0d3fa42472208c6c477728286
Murad-Cavadov <13860@holbertonstudents.com>
2026-05-26 09:10:01 +0000

    Fix: Add task 0-hbtn_status.py

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module fetches https://intranet.hbtn.io/status
+and displays the body of the response.
+"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    req = urllib.request.Request(url, headers={'cfclearance': 'true'})
+
+    with urllib.request.urlopen(req) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
