b22753f100673cad2ffe3590a993828061fbcff9
Murad-Cavadov <13860@holbertonstudents.com>
2026-05-26 09:12:37 +0000

    Fix: Add task 2-post_email.py

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module takes in a URL and an email, sends a POST request
+to the passed URL with the email as a parameter, and displays
+the body of the response (decoded in utf-8).
+"""
+import sys
+import urllib.parse
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    values = {'email': email}
+
+    # Datay-ı URL formatına salıb baytlara çeviririk
+    data = urllib.parse.urlencode(values).encode('utf-8')
+
+    # Sorğunu (Request) hazırlayırıq (həm data, həm də firewall başlığı ilə)
+    req = urllib.request.Request(url, data=data, headers={'cfclearance': 'true'})
+
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
