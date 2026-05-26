fe0f05bd5c97ea3baa987480d4864aad05c8e12c
Murad-Cavadov <13860@holbertonstudents.com>
2026-05-26 11:02:24 +0000

    Fix: pycodestyle W293 trailing whitespaces

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -9,8 +9,7 @@ import requests
 
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
-    
-    # Arqument verilib-verilmədiyini yoxlayırıq
+
     if len(sys.argv) > 1:
         q = sys.argv[1]
     else:
@@ -19,21 +18,16 @@ if __name__ == "__main__":
     payload = {'q': q}
 
     try:
-        # Sorğunu göndəririk (firewall üçün header ilə birgə)
         response = requests.post(
             url, data=payload, headers={'cfclearance': 'true'}
         )
-        
-        # Gələn cavabı JSON olaraq oxumağa çalışırıq
+
         json_data = response.json()
 
-        # JSON etibarlıdırsa, amma boşdursa
         if json_data == {}:
             print("No result")
         else:
-            # Holberton tələbi: lüğətdən məlumatları .get() ilə çəkirik
             print("[{}] {}".format(json_data.get("id"), json_data.get("name")))
 
     except ValueError:
-        # Əgər gələn cavab düzgün JSON formatında deyilsə
         print("Not a valid JSON")
